47bc152059a6cfbd851c8345911083de95994d3f
Tsaqif Mutashim Mufid <Mufidmutashim@gmail.com>
2025-02-10 07:57:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember()
+{
+    var member = {
+        name: "John Doe",
+        age: 25,
+        skills: ["HTML", "CSS", "JS"],
+        details: function() {
+            console.log(this.name + " is " + this.age + " years old.");
+            console.log(this.name + " knows " + this.skills.join(", "));
+        }
+    };
+    member.details();
+}
